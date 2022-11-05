09911437f6d726091cb83bf68bcff4f5ab324d2a
EntityRender <xxayo23xx@gmail.com>
2022-11-05 23:14:56 +1100

    V5

--- onload.js
+++ onload.js
@@ -1,3 +1,7 @@
+function getRandomArbitrary(min, max) {
+    return Math.random() * (max - min) + min;
+  }
+  
 console.log("Stop looking at the console, there is nothing to see here, go away - Faye")
 const sleepNow = (delay) => new Promise((resolve) => setTimeout(resolve, delay))
 async function loader() {
@@ -18,4 +22,55 @@ async function loader() {
             }
 }
 
+randint = Math.floor(getRandomArbitrary(1,10))
+console.log(randint)
+console.log(getRandomArbitrary(1,10))
+async function splashscreen() {
+    if (randint == 1) {
+        await sleepNow(300)
+        document.getElementById("splashscreen").innerHTML = "Now with extra JavaScript!";
+    }
+    else if (randint == 2){
+        await sleepNow(300)
+        document.getElementById("splashscreen").innerHTML = "Copying data!"; 
+    }
+    else if (randint == 3){
+        await sleepNow(300)
+        document.getElementById("splashscreen").innerHTML = "Browsing Stackoverflow!"; 
+    }
+    else if (randint == 4){
+        await sleepNow(300)
+        document.getElementById("splashscreen").innerHTML = "Uninstalling php!"; 
+    }
+    else if (randint == 5){
+        await sleepNow(300)
+        document.getElementById("splashscreen").innerHTML = "Debugging type errors!"; 
+    }
+    else if (randint == 6){
+        await sleepNow(300)
+        document.getElementById("splashscreen").innerHTML = "Deleting pull requests!"; 
+    }
+    else if (randint == 7){
+        await sleepNow(300)
+        document.getElementById("splashscreen").innerHTML = "Ice cream is overrated!"; 
+    }
+    else if (randint == 8){
+        await sleepNow(300)
+        document.getElementById("splashscreen").innerHTML = "osu! < beat sabre!"; 
+    }
+    else if (randint == 9){
+        await sleepNow(300)
+        document.getElementById("splashscreen").innerHTML = "Hosted on Github Pages!"; 
+    }
+    else if (randint == 10){
+        await sleepNow(300)
+        document.getElementById("splashscreen").innerHTML = "No added preservatives!"; 
+    }
+    else{
+        console.log("Error")
+    }
+}
+
+splashscreen()
 loader()
+
