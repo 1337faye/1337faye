9b3e0f28bec6db787f356b9a226852e9cfe13efb
Faye <33893492+1337faye@users.noreply.github.com>
2024-02-28 17:38:52 +1100

    Tailwind fix
    stopped using the tailwind CDN 😭

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html', 'music.html', 'resume.html', 'portfolio.html' ,  'src/**/*.{vue,js,ts,jsx,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
