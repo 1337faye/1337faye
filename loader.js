8c03a186cc39db0fcba3009f1ddb2b771dd04420
Faye <33893492+1337faye@users.noreply.github.com>
2024-02-29 20:56:07 +1100

    Restyle
    Restyled the site & Added a loader

---
+++ loader.js
@@ -0,0 +1,16 @@
+
+  document.addEventListener("DOMContentLoaded", function() {
+    const loader = document.getElementById("loader");
+    loader.style.opacity = 1;
+
+    function fadeOut() {
+      loader.style.opacity -= 0.1;
+      if (loader.style.opacity <= 0) {
+        loader.style.display = "none";
+      } else {
+        setTimeout(requestAnimationFrame(fadeOut), 500);
+      }
+    }
+
+    setTimeout(fadeOut, 500);
+  });
